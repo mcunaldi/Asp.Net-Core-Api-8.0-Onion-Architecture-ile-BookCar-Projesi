be771494e9ebc240844cf91be89639a7af88497b
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-05-12 10:49:06 +0300

    B30 - Dashboard - F

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardBLogListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.BlogDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardComponents;
+
+public class _AdminDashboardBLogListComponentPartial : ViewComponent
+{
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _AdminDashboardBLogListComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [Route("Index")]
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient(); //istemci anlamına geliyor.
+        var responseMessage = await client.GetAsync("https://localhost:7038/api/Blogs/GetAllBlogsWithAuthorList");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
